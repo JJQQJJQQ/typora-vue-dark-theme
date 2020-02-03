918243684e8a85678a4178c99bbc6eba6476d761
MamoruDS <mamoruds.io@gmail.com>
2020-02-04 08:16:23 +0900

    added new codefence color scheme 'oceanicnext'

--- src/merge.sh
+++ src/merge.sh
@@ -2,7 +2,13 @@
 
 rm -f ../vue-dark.css
 rm -f ../vue-dark-snappy.css
+rm -f ../vue-dark-oceanicnext.css
+
 cat dark_code_fence_color_scheme.css >> ../vue-dark.css
 cat vue_dark_fork.css >> ../vue-dark.css
+
 cat ../vue-dark.css >> ../vue-dark-snappy.css
 cat snappy_code_fence_color_scheme.css >> ../vue-dark-snappy.css
+
+cat ../vue-dark.css >> ../vue-dark-oceanicnext.css
+cat codefence_colorscheme_oceanicnext.css >> ../vue-dark-oceanicnext.css
